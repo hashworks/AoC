273e6f68340bf758143a357cd9c3e63849d886f4
hashworks <mail@hashworks.net>
2021-12-01 15:05:12 +0100

    2021-day01: Add std::vec::Vec::windows solution

--- 2021/day01/src/main.rs
+++ 2021/day01/src/main.rs
@@ -15,7 +15,7 @@ fn _part1_slow_zip(measurements: &Vec<usize>) -> usize {
         .count()
 }
 
-fn part1(measurements: &Vec<usize>) -> usize {
+fn _part1_filter(measurements: &Vec<usize>) -> usize {
     let mut p = measurements.first().unwrap_or(&0);
     measurements
         .iter()
@@ -28,14 +28,29 @@ fn part1(measurements: &Vec<usize>) -> usize {
         .count()
 }
 
-fn part2(measurements: &Vec<usize>) -> usize {
-    part1(
+fn _part2_zip(measurements: &Vec<usize>) -> usize {
+    _part1_filter(
         &measurements
             .iter()
             .zip(measurements.iter().skip(1))
             .zip(measurements.iter().skip(2))
             .map(|((m1, m2), m3)| m1 + m2 + m3)
             .collect(),
+    )
+}
+
+// https://doc.rust-lang.org/std/vec/struct.Vec.html#method.windows
+
+fn part1_windows(measurements: &Vec<usize>) -> usize {
+    measurements.windows(2).filter(|&w| w[0] < w[1]).count()
+}
+
+fn part2_windows(measurements: &Vec<usize>) -> usize {
+    part1_windows(
+        &measurements
+            .windows(3)
+            .map(|w| w.iter().sum())
+            .collect::<Vec<usize>>(),
     )
 }
 
@@ -52,14 +67,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let s_part1 = Instant::now();
     println!(
         "part1: {} ({}ns)",
-        part1(&measurements),
+        part1_windows(&measurements),
         s_part1.elapsed().as_nanos()
     );
 
     let s_part2 = Instant::now();
     println!(
         "part2: {} ({}µs)",
-        part2(&measurements),
+        part2_windows(&measurements),
         s_part2.elapsed().as_micros()
     );
 
@@ -68,29 +83,34 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+const _EXAMPLE: [usize; 10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
 #[test]
-fn test_part1_example1() {
-    assert_eq!(0, part1(&[].to_vec()));
-    assert_eq!(
-        7,
-        part1(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263].to_vec())
-    );
+fn test_part1_example() {
+    assert_eq!(0, _part1_filter(&[].to_vec()));
+    assert_eq!(7, _part1_filter(&_EXAMPLE.to_vec()));
 }
 
 #[test]
-fn test_part1_slow_zip_example1() {
+fn test_part1_slow_zip_example() {
     assert_eq!(0, _part1_slow_zip(&[].to_vec()));
-    assert_eq!(
-        7,
-        _part1_slow_zip(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263].to_vec())
-    );
+    assert_eq!(7, _part1_slow_zip(&_EXAMPLE.to_vec()));
 }
 
 #[test]
-fn test_part2_example1() {
-    assert_eq!(0, part2(&[].to_vec()));
-    assert_eq!(
-        5,
-        part2(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263].to_vec())
-    );
+fn test_part2_example() {
+    assert_eq!(0, _part2_zip(&[].to_vec()));
+    assert_eq!(5, _part2_zip(&_EXAMPLE.to_vec()));
+}
+
+#[test]
+fn test_part1_windows_example() {
+    assert_eq!(0, part1_windows(&[].to_vec()));
+    assert_eq!(7, part1_windows(&_EXAMPLE.to_vec()));
+}
+
+#[test]
+fn test_part2_windows_example() {
+    assert_eq!(0, part2_windows(&[].to_vec()));
+    assert_eq!(5, part2_windows(&_EXAMPLE.to_vec()));
 }
