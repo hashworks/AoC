874297ccee830b6e9747230ec98775d2f36d0cee
hashworks <mail@hashworks.net>
2022-12-01 20:40:48 +0100

    Add missing 2022 files

---
+++ 2022/src/util/input.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Result as IoResult;
+
+#[allow(dead_code)]
+pub(crate) fn get_reader(id: &str) -> IoResult<BufReader<File>> {
+    let path = match env::var("INPUT") {
+        Ok(val) => val,
+        Err(_) => format!("inputs/{}.txt", id),
+    };
+    Ok(BufReader::new(File::open(path.clone())?))
+}
