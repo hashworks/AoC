89f8ab8cc20fbdec03437b16c67818dfcfbf836e
hashworks <mail@hashworks.net>
2023-12-01 07:36:48 +0100

    Improve 2023-day01

--- 2023/src/day01.rs
+++ 2023/src/day01.rs
@@ -5,7 +5,7 @@ use util::{aoc::AoCDay, input::get_reader};
 
 const ID: &str = "day01";
 type Input = Vec<Vec<char>>;
-type Output = u64;
+type Output = usize;
 
 struct Day {}
 
@@ -23,116 +23,71 @@ impl AoCDay<Input, Output> for Day {
     }
 
     fn part1(&self, input: &Input) -> Result<Output, Box<dyn Error>> {
-        let mut digits = Vec::new();
+        let mut sum = 0;
         for line in input {
-            let mut chars = Vec::new();
-            for c in line {
-                if c.is_ascii_digit() {
-                    chars.push(c);
-                    break;
-                }
-            }
-            for c in line.iter().rev() {
-                if c.is_ascii_digit() {
-                    chars.push(c);
-                    break;
-                }
-            }
-            digits.push(chars.iter().cloned().collect::<String>().parse()?);
+            sum += 10
+                * line
+                    .iter()
+                    .filter(|c| c.is_ascii_digit())
+                    .map(|c| *c as usize - 48)
+                    .next()
+                    .unwrap_or(0)
+                + line
+                    .iter()
+                    .filter(|c| c.is_ascii_digit())
+                    .map(|c| *c as usize - 48)
+                    .last()
+                    .unwrap_or(0);
         }
-
-        Ok(digits.iter().sum())
+        Ok(sum)
     }
 
     fn part2(&self, input: &Input) -> Result<Output, Box<dyn Error>> {
-        let mut digits = Vec::new();
+        let mut sum = 0;
+
         for line in input {
-            let mut parsed_line = Vec::new();
+            let mut parsed_digits = Vec::new();
 
             let mut i = 0;
             while i < line.len() {
                 if line[i].is_ascii_digit() {
-                    parsed_line.push(line[i]);
+                    parsed_digits.push(line[i] as usize - 48);
                 } else {
                     if i >= 2 {
-                        if line[i - 2] == 'o' && line[i - 1] == 'n' && line[i] == 'e' {
-                            parsed_line.push('1');
-                        } else if line[i - 2] == 't' && line[i - 1] == 'w' && line[i] == 'o' {
-                            parsed_line.push('2');
-                        } else if line[i - 2] == 's' && line[i - 1] == 'i' && line[i] == 'x' {
-                            parsed_line.push('6');
+                        if line[i - 2..=i] == ['o', 'n', 'e'] {
+                            parsed_digits.push(1);
+                        } else if line[i - 2..=i] == ['t', 'w', 'o'] {
+                            parsed_digits.push(2);
+                        } else if line[i - 2..=i] == ['s', 'i', 'x'] {
+                            parsed_digits.push(6);
                         }
                     }
                     if i >= 3 {
-                        if line[i - 3] == 'f'
-                            && line[i - 2] == 'o'
-                            && line[i - 1] == 'u'
-                            && line[i] == 'r'
-                        {
-                            parsed_line.push('4');
-                        }
-                        if line[i - 3] == 'f'
-                            && line[i - 2] == 'i'
-                            && line[i - 1] == 'v'
-                            && line[i] == 'e'
-                        {
-                            parsed_line.push('5');
-                        }
-                        if line[i - 3] == 'n'
-                            && line[i - 2] == 'i'
-                            && line[i - 1] == 'n'
-                            && line[i] == 'e'
-                        {
-                            parsed_line.push('9');
+                        if line[i - 3..=i] == ['f', 'o', 'u', 'r'] {
+                            parsed_digits.push(4);
+                        } else if line[i - 3..=i] == ['f', 'i', 'v', 'e'] {
+                            parsed_digits.push(5);
+                        } else if line[i - 3..=i] == ['n', 'i', 'n', 'e'] {
+                            parsed_digits.push(9);
                         }
                     }
                     if i >= 4 {
-                        if line[i - 4] == 't'
-                            && line[i - 3] == 'h'
-                            && line[i - 2] == 'r'
-                            && line[i - 1] == 'e'
-                            && line[i] == 'e'
-                        {
-                            parsed_line.push('3');
-                        } else if line[i - 4] == 's'
-                            && line[i - 3] == 'e'
-                            && line[i - 2] == 'v'
-                            && line[i - 1] == 'e'
-                            && line[i] == 'n'
-                        {
-                            parsed_line.push('7');
-                        } else if line[i - 4] == 'e'
-                            && line[i - 3] == 'i'
-                            && line[i - 2] == 'g'
-                            && line[i - 1] == 'h'
-                            && line[i] == 't'
-                        {
-                            parsed_line.push('8');
+                        if line[i - 4..=i] == ['t', 'h', 'r', 'e', 'e'] {
+                            parsed_digits.push(3);
+                        } else if line[i - 4..=i] == ['s', 'e', 'v', 'e', 'n'] {
+                            parsed_digits.push(7);
+                        } else if line[i - 4..=i] == ['e', 'i', 'g', 'h', 't'] {
+                            parsed_digits.push(8);
                         }
                     }
                 }
-
                 i += 1;
             }
 
-            let mut chars = Vec::new();
-            for c in &parsed_line {
-                if c.is_ascii_digit() {
-                    chars.push(*c);
-                    break;
-                }
-            }
-            for c in parsed_line.iter().rev() {
-                if c.is_ascii_digit() {
-                    chars.push(*c);
-                    break;
-                }
-            }
-
-            digits.push(chars.iter().cloned().collect::<String>().parse()?);
+            sum += 10 * parsed_digits[0] + parsed_digits.last().unwrap_or(&0);
         }
 
-        Ok(digits.iter().sum())
+        Ok(sum)
     }
 }
 
