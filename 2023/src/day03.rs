fe8f1cf119e6b637cb4b5fc049e36ef75fc872c2
hashworks <mail@hashworks.net>
2023-12-03 08:55:27 +0100

    Improve 2023-day03

--- 2023/src/day03.rs
+++ 2023/src/day03.rs
@@ -15,6 +15,7 @@ impl AoCDay<Input, Output> for Day {
         let reader = get_reader(id);
 
         let mut input = Vec::new();
+
         for line in reader?.lines() {
             let line = line?;
             input.push(line.chars().collect());
@@ -28,9 +29,10 @@ impl AoCDay<Input, Output> for Day {
 
         for (y, line) in input.iter().enumerate() {
             let mut incomplete_number = Vec::new();
+
             for (x, c) in line.iter().enumerate() {
                 let (x, end_of_line) = if c.is_ascii_digit() {
-                    incomplete_number.push(*c);
+                    incomplete_number.push(*c as usize - 48);
 
                     // Edge case: We are at the end of the line, so we asume we are in the next column
                     if x + 1 == line.len() {
@@ -43,32 +45,21 @@ impl AoCDay<Input, Output> for Day {
                 };
 
                 if !incomplete_number.is_empty() && (!c.is_ascii_digit() || end_of_line) {
-                    // We could calculate this, but I'm lazy
-                    let number = incomplete_number
-                        .iter()
-                        .collect::<String>()
-                        .parse::<usize>()
-                        .unwrap();
+                    let number = digit_slice_to_number(&incomplete_number);
 
                     // Check left and right of number
                     if (x - incomplete_number.len() > 0
-                        && input[y]
-                            .get(x - incomplete_number.len() - 1)
-                            .unwrap_or(&'.')
-                            != &'.')
+                        && input[y][x - incomplete_number.len() - 1] != '.')
                         || input[y].get(x).unwrap_or(&'.') != &'.'
                     {
                         part_numbers.push(number);
                     } else {
-                        let left = if x - incomplete_number.len() > 0 {
-                            x - incomplete_number.len() - 1
-                        } else {
-                            x - incomplete_number.len()
-                        };
+                        let left = x - incomplete_number.len();
+                        let left = if left > 0 { left - 1 } else { left };
 
                         let right = if x == line.len() { x - 1 } else { x };
 
-                        // Check over number
+                        // Check over and under number
                         if (y > 0
                             && input[y - 1][left..=right]
                                 .iter()
@@ -96,9 +87,10 @@ impl AoCDay<Input, Output> for Day {
 
         for (y, line) in input.iter().enumerate() {
             let mut incomplete_number = Vec::new();
+
             for (x, c) in line.iter().enumerate() {
                 let (x, end_of_line) = if c.is_ascii_digit() {
-                    incomplete_number.push(*c);
+                    incomplete_number.push(*c as usize - 48);
 
                     // Edge case: We are at the end of the line, so we asume we are in the next column
                     if x + 1 == line.len() {
@@ -111,68 +103,40 @@ impl AoCDay<Input, Output> for Day {
                 };
 
                 if !incomplete_number.is_empty() && (!c.is_ascii_digit() || end_of_line) {
-                    // We could calculate this, but I'm lazy
-                    let number = incomplete_number
-                        .iter()
-                        .collect::<String>()
-                        .parse::<usize>()
-                        .unwrap();
+                    let number = digit_slice_to_number(&incomplete_number);
 
                     // Check left of number
                     if x - incomplete_number.len() > 0
-                        && input[y]
-                            .get(x - incomplete_number.len() - 1)
-                            .unwrap_or(&'.')
-                            == &'*'
+                        && input[y][x - incomplete_number.len() - 1] == '*'
                     {
-                        if let Some(other) =
-                            gear_map.insert((x - incomplete_number.len() - 1, y), number)
-                        {
-                            gear_sum += number * other;
-                        }
+                        gear_sum += number
+                            * gear_map
+                                .insert((x - incomplete_number.len() - 1, y), number)
+                                .unwrap_or(0);
                     // Check right of number
                     } else if input[y].get(x).unwrap_or(&'.') == &'*' {
-                        if let Some(other) = gear_map.insert((x, y), number) {
-                            gear_sum += number * other;
-                        }
+                        gear_sum += number * gear_map.insert((x, y), number).unwrap_or(0);
                     } else {
-                        let left = if x - incomplete_number.len() > 0 {
-                            x - incomplete_number.len() - 1
-                        } else {
-                            x - incomplete_number.len()
-                        };
+                        let left = x - incomplete_number.len();
+                        let left = if left > 0 { left - 1 } else { left };
 
                         let right = if x == line.len() { x - 1 } else { x };
 
                         // Check over number
                         if y > 0 && input[y - 1][left..=right].iter().any(|c| c == &'*') {
-                            let star_x = left
-                                + input[y - 1][left..=right]
-                                    .iter()
-                                    .enumerate()
-                                    .filter(|(_, c)| c == &&'*')
-                                    .map(|(i, _)| i)
-                                    .next()
-                                    .unwrap();
-                            if let Some(other) = gear_map.insert((star_x, y - 1), number) {
-                                gear_sum += number * other;
-                            }
+                            let star_x =
+                                left + get_star_position(&input[y - 1][left..=right]).unwrap();
+                            gear_sum +=
+                                number * gear_map.insert((star_x, y - 1), number).unwrap_or(0);
 
                         // Check below number
                         } else if y + 1 < input.len()
                             && input[y + 1][left..=right].iter().any(|c| c == &'*')
                         {
-                            let star_x = left
-                                + input[y + 1][left..=right]
-                                    .iter()
-                                    .enumerate()
-                                    .filter(|(_, c)| c == &&'*')
-                                    .map(|(i, _)| i)
-                                    .next()
-                                    .unwrap();
-                            if let Some(other) = gear_map.insert((star_x, y + 1), number) {
-                                gear_sum += number * other;
-                            }
+                            let star_x =
+                                left + get_star_position(&input[y + 1][left..=right]).unwrap();
+                            gear_sum +=
+                                number * gear_map.insert((star_x, y + 1), number).unwrap_or(0);
                         }
                     }
 
@@ -183,6 +147,24 @@ impl AoCDay<Input, Output> for Day {
 
         Ok(gear_sum)
     }
+}
+
+fn get_star_position(slice: &[char]) -> Option<usize> {
+    slice
+        .iter()
+        .enumerate()
+        .filter(|(_, c)| c == &&'*')
+        .map(|(i, _)| i)
+        .next()
+}
+
+fn digit_slice_to_number(slice: &[usize]) -> usize {
+    slice
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, d)| d * 10usize.pow(i as u32))
+        .sum()
 }
 
 fn main() {
@@ -211,5 +193,21 @@ mod tests {
                 .unwrap(),
             467835
         );
+    }
+
+    #[test]
+    fn test_digit_list_to_number() {
+        assert_eq!(digit_slice_to_number(&[1]), 1);
+        assert_eq!(digit_slice_to_number(&[1, 2]), 12);
+        assert_eq!(digit_slice_to_number(&[1, 2, 3]), 123);
+    }
+
+    #[test]
+    fn test_get_star_position() {
+        assert_eq!(get_star_position(&[]), None);
+        assert_eq!(get_star_position(&['a']), None);
+        assert_eq!(get_star_position(&['*']), Some(0));
+        assert_eq!(get_star_position(&['a', '*']), Some(1));
+        assert_eq!(get_star_position(&['a', '*', 'b']), Some(1));
     }
 }
