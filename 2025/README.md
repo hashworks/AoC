9ce381606cb53f4998cad80d31fbab3f12a2c3e2
hashworks <mail@hashworks.net>
2025-12-03 07:53:07 +0100

    Add 2025-day03 solutions

--- 2025/README.md
+++ 2025/README.md
@@ -28,7 +28,7 @@ Times are normalized from input download to correct answer, not the time on the 
 | ---: | -------: | -------: | ----: |
 |   01 | 00:27:44 | 01:01:44 |       |
 |   02 | 00:41:42 | 02:00:37 |       |
-|   03 |          |          |       |
+|   03 | 00:13:35 | 01:09:11 |       |
 |   04 |          |          |       |
 |   05 |          |          |       |
 |   06 |          |          |       |
@@ -45,9 +45,9 @@ Please note that the following numbers are relative, and should only be used to 
 
 |  Day |  Parsing |  Part 1 |   Part 2 | Notes |
 | ---: | -------: | ------: | -------: | ----: |
-|   01 | 529.46µs | 33.46µs |   1.28ms |       |
-|   02 |  30.84µs | 40.75ms | 147.19ms |       |
-|   03 |          |         |          |       |
+|   01 | 333.72µs | 15.47µs | 443.90µs |       |
+|   02 |  22.52µs | 30.45ms | 126.34ms |       |
+|   03 | 165.15µs | 15.73µs |  39.98µs |       |
 |   04 |          |         |          |       |
 |   05 |          |         |          |       |
 |   09 |          |         |          |       |
---
+++ 2025/src/day03.rs
@@ -0,0 +1,98 @@
+mod util;
+
+use std::{error::Error, io::BufRead};
+use util::{aoc::AoCDay, input::get_reader};
+
+const ID: &str = "day03";
+type Input = Vec<Vec<u32>>;
+type Output = usize;
+
+struct Day {}
+
+impl AoCDay<Input, Output> for Day {
+    fn parse_input(&self, id: &str) -> Result<Input, Box<dyn Error>> {
+        let reader = get_reader(id)?;
+
+        reader
+            .lines()
+            .into_iter()
+            .map(|l| {
+                l?.chars()
+                    .map(|c| {
+                        c.to_digit(10).ok_or_else(|| {
+                            Box::<dyn Error>::from("Input char is not a valid digit")
+                        })
+                    })
+                    .collect()
+            })
+            .collect()
+    }
+
+    fn part1(&self, input: &Input) -> Result<Output, Box<dyn Error>> {
+        Ok(joltage_maximizer(input, 2))
+    }
+
+    fn part2(&self, input: &Input) -> Result<Output, Box<dyn Error>> {
+        Ok(joltage_maximizer(input, 12))
+    }
+}
+
+fn joltage_maximizer(banks: &Input, digits: usize) -> Output {
+    banks
+        .iter()
+        .map(|bank| {
+            let max_skips = bank.len() - (digits - 1);
+            (0..digits)
+                .fold((0, 0), |(bank_sum, skips), pos| {
+                    let mut pos_max_i = 0;
+                    let mut pos_max = 0;
+                    for (current_pos_n_i, pos_n) in bank
+                        .iter()
+                        .skip(pos + skips)
+                        .take(max_skips - skips)
+                        .enumerate()
+                    {
+                        if *pos_n > pos_max {
+                            pos_max_i = current_pos_n_i;
+                            pos_max = *pos_n;
+                        }
+                    }
+
+                    (
+                        bank_sum + 10usize.pow(digits as u32 - 1 - pos as u32) * pos_max as usize,
+                        skips + pos_max_i,
+                    )
+                })
+                .0
+        })
+        .sum()
+}
+
+fn main() {
+    Day {}.run(ID);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_part1() {
+        let day = Day {};
+        assert_eq!(
+            day.parse_and_solve_part1(format!("{}_test1", ID).as_str())
+                .unwrap(),
+            357
+        );
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        let day = Day {};
+        assert_eq!(
+            day.parse_and_solve_part2(format!("{}_test1", ID).as_str())
+                .unwrap(),
+            3121910778619
+        );
+    }
+}
